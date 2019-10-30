f5cead15f8e227e03acb823ab9bdbafc325ce31f
Ajay Mall <mallajay19@gmail.com>
2019-10-30 14:03:49 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+//add this file to .gitignore
+
+module.exports = {
+    google: {
+    clientID: '26302738426-gfm0qaf1kfpdglfq6nhbdjfp45k2ked3.apps.googleusercontent.com',
+    clientSecret: 'SOL167aNFllDGpXIXDJD_aBc'
+    },
+    mongodb: {
+        dbURI: 'mongodb+srv://mallajay:WORLDwar2@cluster0-xbarg.mongodb.net/test?retryWrites=true&w=majority'
+    },
+    session: {
+        cookieKey: 'theSessionCookiekey'
+    }
+};
